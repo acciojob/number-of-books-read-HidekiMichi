b5c4c4c33e5b3444975edbc90490eac13d15dec9
HidekiMichi <105813974+HidekiMichi@users.noreply.github.com>
2024-01-31 04:52:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let readBooks=library.filter((obj)=>{
+		return obj.readingStatus==true;
+	});
+	return readBooks.length;
 };
 
 // Do not change the code below
